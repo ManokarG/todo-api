4f29dba290eb1df647de0ad9fad0a2c98b0f0540
manokarg <cmano.gar04@gmail.com>
2017-02-07 07:36:50 +0530

    Postgresql added to heroku

--- db.js
+++ db.js
@@ -1,10 +1,18 @@
-var Sequelize=require('sequelize');
-var sequelize=new Sequelize(undefined,undefined,undefined,{
+const Sequelize=require('sequelize');
+const env=process.env.NODE_DEV||'development';
+if(env=='production'){
+const sequelize=new Sequelize(process.env.DATABASE_URL,{
+	'dialect':'postgres',
+	'storage':__dirname+'/data/db-sqlite-api.sqlite'
+});
+}else{
+const sequelize=new Sequelize(undefined,undefined,undefined,{
 	'dialect':'sqlite',
 	'storage':__dirname+'/data/db-sqlite-api.sqlite'
 });
+}
 
-var db={};
+const db={};
 
 db.todo=sequelize.import(__dirname+'/models/todo.js');
 db.sequelize=sequelize;
